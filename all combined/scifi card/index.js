856ec72d838ae25a4eacd637c6b4a0ca4819ee6a
DedoCvele <93141970+DedoCvele@users.noreply.github.com>
2023-05-08 16:13:48 +0200

    Add files via upload

---
+++ all combined/scifi card/index.js
@@ -0,0 +1,45 @@
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
+
+
+/* TEXT GENERATOR */
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+
+let interval = null;
+
+const screen = document.querySelector(".screen"),
+      name = document.querySelector(".name");
+
+screen.onmouseenter = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    name.innerText = name.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return name.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 27)]
+      })
+      .join("");
+    
+    if(iteration >= name.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
